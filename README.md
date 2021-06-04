4fea705b29a83c2cdf57d8b0b2a44e8dc4224ffc
Aiden <marcin.madry@interia.com>
2021-06-04 20:17:00 +0200

    Added close functionality

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Simple-Lightbox
 
-Small JavaScript library/framework, mostly for personal use, used to create small galleries for simple websites. 80% of pre-existing ones, claiming to be "lightweight" and "simple" actually have over 800 lines of code PogO
+Small JavaScript library/framework, mostly for personal use, I use it to create small galleries for simple websites. 80% of pre-existing ones, claiming to be "lightweight" and "simple" actually have over 800 lines of code PogO
 
 ## Quick start/Instructions
 
 I don't know why you would want to use it... but a simple demo can be found in "[index.html](https://github.com/Zermil/simple-lightbox/blob/master/index.html)" it also has "customizable" CSS so suit yourself. 
 
-**NOTE** that it needs some sort of "server" to run, because of XML request.
+**NOTE** that it needs some sort of "server" to run (website needs to be hosted somewhere), because of XML request.
 
 ```html
 <!DOCTYPE html>
@@ -23,7 +23,7 @@ I don't know why you would want to use it... but a simple demo can be found in "
 <script>
   window.onload = () => {
     
-    createLightbox({
+    createGallery({
       target: "gallery",
       photos_dir: "./photos"
     });
--- lightbox.js
+++ lightbox.js
@@ -1,16 +1,77 @@
+function removeLightbox() {
+  document.body.removeChild(document.querySelector(".lightboxLB"));
+  document.body.removeChild(document.querySelector(".lightbox-image-containerLB"));
 
-function createPhotoElement(src, name) {
+  removeEventListener("keydown", listenerRemoveLightbox);
+}
+
+function listenerRemoveLightbox(e) {
+  if (e.key === "Escape") removeLightbox();
+}
+
+function controlPanelComponent() {
+  const controlPanel = document.createElement("div");
+  const closeButton = document.createElement("span");
+
+  controlPanel.classList.add("lightbox-controlLB");
+  closeButton.classList.add("close-button");
+  closeButton.textContent = "X";
+
+  closeButton.onclick = removeLightbox;
+
+  controlPanel.appendChild(closeButton);
+  return controlPanel;
+}
+
+function createLightboxElement(element) {
+  //TODO: Make it customizable(?)
+  const scalar = 0.756;
+  
+  //IMAGE
+  const imageContainer = document.createElement("div");
+  const image = document.createElement("img");
+
+  imageContainer.classList.add("lightbox-image-containerLB");
+
+  image.classList.add("custom-photo-enlargeLB");
+  image.setAttribute("src", element.img);
+
+  image.style.maxWidth = `${screen.width * scalar}px`;
+  image.style.maxHeight = `${screen.height * scalar}px`;
+
+  imageContainer.appendChild(image);
+  //================
+
+  //LIGHTBOX
+  const lightbox = document.createElement("div");
+  lightbox.classList.add("lightboxLB");
+
+  lightbox.onclick = removeLightbox;
+  addEventListener("keydown", listenerRemoveLightbox);
+  //================
+
+  //CONTROL
+  imageContainer.appendChild(controlPanelComponent());
+  //================
+  
+  document.body.appendChild(lightbox);
+  document.body.appendChild(imageContainer);
+}
+
+function createPhotoElement(element) {
   const photoObject = document.createElement("img");
 
   photoObject.classList.add("custom-photoLB");
 
-  photoObject.setAttribute("src", src);
-  photoObject.setAttribute("alt", `lightbox_${name}`);
+  photoObject.setAttribute("src", element.img);
+  photoObject.setAttribute("alt", `lightbox_${element.name}`);
+
+  photoObject.onclick = () => createLightboxElement(element);
 
   return photoObject;
 }
 
-function createLightbox(properties) {
+function createGallery(properties) {
   const target = document.getElementById(properties.target || "gallery");
   let dir = properties.photos_dir || "./photos/";
 
@@ -18,23 +79,27 @@ function createLightbox(properties) {
     console.error("Could not find specified target location: Invalid gallery/target id");
     return;
   }
-
+  
+  //Why is web-development like this...
   const xhr = new XMLHttpRequest();
   xhr.open("GET", dir, true);
   xhr.responseType = 'document';
   
   xhr.onload = function() {
     if (xhr.status === 200) {
-      const elements = xhr.response.getElementsByTagName("a");
-      
+      const elements = 
+        [...xhr.response.getElementsByTagName("a")]
+        .filter(photo => photo.href.match(/\.(jpe?g|png)$/))
+        .map((photo, index) => photo = {
+          index,
+          img: photo.href,
+          name: photo.title
+        });
+
       for (element of elements) {
-        if ( element.href.match(/\.(jpe?g|png)$/) ) {
-
-          target.appendChild(
-            createPhotoElement(element.href, element.title)
-          );
-
-        }
+        target.appendChild(
+          createPhotoElement(element)
+        );
       }
     } else {
       console.error("Failed to fetch photos from folder/specified path", xhr.status);
